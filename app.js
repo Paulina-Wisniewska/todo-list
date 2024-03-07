290e01ac437a40c85f8205f725aac7a67c694f29
Paulina Wisniewska <paulina10346@gmail.com>
2024-03-07 10:43:32 +0100

    remove local todos

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ function addTodo(event){
         if(item.classList[0] === 'trash-btn'){
             const todo = item.parentElement;
             todo.classList.add('fall');
+            removeLocalTodos(todo);
             todo.addEventListener('transitionend', function(){
                 todo.remove(); 
             });
@@ -118,4 +119,16 @@ function getTodos(){
 
     todoList.appendChild(todoDiv);
     });
+}
+
+function removeLocalTodos(todo){
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
